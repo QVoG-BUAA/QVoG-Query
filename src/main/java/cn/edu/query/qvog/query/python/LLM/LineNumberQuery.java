ee779fc8ba9f6bc09e9da6a476b2fe6c6adc85bc
GuoLan <2747525625@qq.com>
2025-06-11 11:44:14 +0800

    add LLMQuery, modify LineNumberQuery

--- src/main/java/cn/edu/query/qvog/query/python/LLM/LineNumberQuery.java
+++ src/main/java/cn/edu/query/qvog/query/python/LLM/LineNumberQuery.java
@@ -29,7 +29,7 @@ public class LineNumberQuery extends PythonQuery {
 
         for (int i = 0; i < length; i++) {
             // 注意：这里假设JSONArray中的所有元素都是数字
-            intArray[i] = Integer.parseInt(jsonArray.get(i).toString()) + 1;
+            intArray[i] = Integer.parseInt(jsonArray.get(i).toString());
         }
 
         return intArray;
@@ -39,25 +39,22 @@ public class LineNumberQuery extends PythonQuery {
     public CompleteQuery run() {
         try {
             JSONObject config = JsonHelper.load("./config.json");
-            JSONObject llm = JsonHelper.tryGetObject(config, "llm");
+            JSONObject lineno = JsonHelper.tryGetObject(config, "lineno");
 
-            String path = JsonHelper.tryGetValue(llm, "path");
-            String cwe = JsonHelper.tryGetValue(llm, "cwe");
-            String file = JsonHelper.tryGetValue(llm, "file");
-
-            JSONObject json = JsonHelper.load(path);
-            JSONObject cweJson = JsonHelper.tryGetObject(json, cwe);
-            JSONObject fileJson = JsonHelper.tryGetObject(cweJson, file);
-            JSONArray source = JsonHelper.tryGetArray(fileJson, "pred_source");
-            JSONArray sink = JsonHelper.tryGetArray(fileJson, "pred_sink");
+            JSONArray source = JsonHelper.tryGetArray(lineno, "source");
+            JSONArray sink = JsonHelper.tryGetArray(lineno, "sink");
+            JSONArray barrier = JsonHelper.tryGetArray(lineno, "barrier");
             System.out.println(source.toString());
             System.out.println(sink.toString());
+            System.out.println(barrier.toString());
             return QueryDescriptor.open()
                     .from("source", new LineNumberMatch(convertJSONArrayToIntArray(source)))
                     .from("sink", new LineNumberMatch(convertJSONArrayToIntArray(sink)))
+                    .from("barrier", new LineNumberMatch(convertJSONArrayToIntArray(barrier)))
                     .where(TaintFlowPredicate.with()
                             .source("source")
                             .sink("sink")
+                            .barrier("barrier")
                             .as("path").exists())
                     .select("source", "sink", "path");
         } catch (Exception e) {
